f505f696a6cbf7fee4ded069e6db7e8c11c73a02
fbl11 <freyab_london@yahoo.co.uk>
2018-09-10 14:53:23 +0100

    Balance returns 0 on new oystercards

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,6 +7,9 @@ describe Oystercard do
     it "returns the @balance on the Oystercard" do
       expect(subject).to respond_to(:balance)
     end
+    it 'returns an initial @balance of 0' do
+      expect(subject.balance).to eq(0)
+    end
 
   end
 
