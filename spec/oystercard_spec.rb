fa9c9fd45700ee91e4ab7d44bcbe1c8b3f8a2474
Nathan Quayle <nathan.quayle@gmail.com>
2018-09-10 15:20:05 +0100

    refactored to use change by

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -13,8 +13,8 @@ describe Oystercard do
   describe "#top_up" do
 
     it 'updates the balance when passed a value' do
-      subject.top_up(10)
-      expect(subject.balance).to eq(10)
+      value = rand(1..50)
+      expect{subject.top_up(value)}.to change{subject.balance}.by(value)
     end
 
   end
