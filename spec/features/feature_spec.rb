212485eace008958e4d7963edd2114565dcc2e47
fbl11 <freyab_london@yahoo.co.uk>
2018-09-11 12:54:47 +0100

    adds user stories 4+5 to feature_spec.rb

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -1,6 +1,7 @@
 describe 'User Stories' do
   let(:card) { Oystercard.new } # globally sets 'card = Oystercard.new' so it does not have to be declared in each example
   MAXIMUM_BALANCE = 90 # explicitly declared here instead of using oystercard.rb constant to flag up if constant has been changed in oystercard.rb
+ 
   # In order to use public transport
   # As a customer
   # I want money on my card
@@ -20,6 +21,26 @@ describe 'User Stories' do
   # As a customer
   # I want a maximum limit (of £90) on my card
   it 'to protect from theft, Oyster balance should be capped' do
-    expect { card.top_up(MAXIMUM_BALANCE + 1) }.to raise_error
+    expect { card.top_up(MAXIMUM_BALANCE + 1) }.to raise_error("Cannot top up: exceeds cap of £#{MAXIMUM_BALANCE}")
   end
+
+  # In order to pay for my journey
+  # As a customer
+  # I need my fare deducted from my card
+  it 'to pay for journey, fare needs to be deducted from Oyster balance' do
+     card.top_up(50)
+     fare = rand(1..5)
+     expect { card.deduct(fare) }.to change { card.balance }.by(-fare)
+  end
+
+  # In order to get through the barriers.
+  # As a customer
+  # I need to touch in and out.
+  it 'to pass barriers, Oysters can be touched in an out' do
+    expect { card.to respond_to(:touch_in) }
+    expect { card.to respond_to(:touch_out) }
+    expect { card.to respond_to(:in_journey?) }
+    end
+  end
+
 end
