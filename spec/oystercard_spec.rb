506f9382fb28f836e7ff9092e00daa6173b63365
fbl11 <freyab_london@yahoo.co.uk>
2018-09-13 14:02:18 +0100

    Completes Step 12, some refactoring

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,46 +4,48 @@ describe Oystercard do
   subject(:oyster) { described_class.new }
   let(:station) { double :station }
 
-  describe "#balance" do
-
-    it 'returns an initial @balance of 0' do
-      expect(oyster.balance).to eq(0)
+  context 'Oyster @balance is empty' do
+   
+    describe "#balance" do
+      it 'A new oyster has a @balance of 0' do
+        expect(oyster.balance).to eq(0)
+      end
+  
     end
-
+  
+    describe "#top_up" do
+  
+      it 'increases @balance by #top_up amount' do
+        amount = rand(1..50)
+        expect { oyster.top_up(amount) }.to change { oyster.balance }.by(amount)
+      end
+  
+      it 'does not permit @balance to go beyond MAXIMUM_BALANCE' do
+        amount = Oystercard::MAXIMUM_BALANCE + 1
+        expect { oyster.top_up(amount) }.to raise_error("Cannot top up: exceeds cap of £#{Oystercard::MAXIMUM_BALANCE}")
+      end
+  
+    end
+  
+    describe '#touch_in' do
+  
+      it 'raises error when @balance is below MINIMUM_BALANCE' do
+        expect { oyster.touch_in(station)}.to raise_error("Cannot touch in: balance below £#{Oystercard::MINIMUM_BALANCE}")
+      end
+  
+    end
+  
+    describe '#touch_out' do
+  
+      it 'deducts the minimum fare from Oyster' do
+        fare = Oystercard::MINIMUM_FARE
+        expect { oyster.touch_out(station) }.to change { oyster.balance }.by(-fare)
+      end
+  
+    end
+  
   end
-
-  describe "#top_up" do
-
-    it 'increases @balance by #top_up amount' do
-      amount = rand(1..50)
-      expect { oyster.top_up(amount) }.to change { oyster.balance }.by(amount)
-    end
-
-    it 'does not permit @balance to go beyond MAXIMUM_BALANCE' do
-      amount = Oystercard::MAXIMUM_BALANCE + 1
-      expect { oyster.top_up(amount) }.to raise_error("Cannot top up: exceeds cap of £#{Oystercard::MAXIMUM_BALANCE}")
-    end
-
-  end
-
-  describe '#touch_in' do
-
-    it 'raises error when @balance is below MINIMUM_BALANCE' do
-      expect { oyster.touch_in(station)}.to raise_error("Cannot touch in: balance below £#{Oystercard::MINIMUM_BALANCE}")
-    end
-
-  end
-
-  describe '#touch_out' do
-
-    it 'deducts the minimum fare from Oyster' do
-      fare = Oystercard::MINIMUM_FARE
-      expect { oyster.touch_out(station) }.to change { oyster.balance }.by(-fare)
-    end
-
-  end
-
-
+  
 
   context 'Oyster is topped up to MAXIMUM BALANCE' do
 
